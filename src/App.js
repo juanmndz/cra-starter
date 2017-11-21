b4465557d83c8cd3578dcb7d8d26c3308063e86f
Juan Mendez <juanmndz19@gmail.com>
2017-11-21 07:19:28 -0700

    Add istanbul and configure correct

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import './App.css';
 
-const App = () => <div>App</div>;
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Welcome to React App</h2>
+    </div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
